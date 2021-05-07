c2fd682b60e30d91f1bd38a8e35c16e7bd628266
aliho <a.hodavand@yahoo.com>
2021-05-07 09:33:09 +0430

    add user model controller, its policy, and its routes

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserUpdateRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Support\Facades\Cache;
+
+class UserController extends Controller
+{
+    public function __construct()
+    {
+        // check user has token (logged in) to update and destroy
+        $this->middleware('auth:api')->only(['update', 'destroy']);
+    }
+
+    public function show($user)
+    {
+        return Cache::tags('users')->remember("user-{$user}", now()->addMinutes(5), function () use ($user) {
+            return new UserResource(User::with(['books', 'comments'])->findOrFail($user));
+        });
+    }
+
+    public function update(UserUpdateRequest $request, User $user)
+    {
+        // authorize user to update action
+        $this->authorize('update', $user);
+        // update user record with request's data
+        $user->update($request->all());
+        // return successful message
+        return response()->json([
+            'message' => 'Update successfully',
+            'data' => new UserResource($user)
+        ]);
+    }
+
+    public function destroy(User $user)
+    {
+        // authorize user to destroy action
+        $this->authorize('delete', $user);
+        // delete user's tokens
+        $user->tokens()->delete();
+        // delete user record
+        $user->delete();
+
+        return response()->json(['message' => 'User deleted successfully']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserAuthController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', [UserAuthController::class, 'register'])->name('register');
 Route::post('login', [UserAuthController::class, 'login'])->name('login');
 Route::post('logout', [UserAuthController::class, 'logout'])->name('logout')->middleware('auth:api');
+
+
+// user model routes
+Route::apiResource('user', UserController::class)->except(['index', 'store']);
