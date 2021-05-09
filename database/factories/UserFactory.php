e0aadccee4e6d6e8aeda3eb3ac25a0939d30acf7
aliho <a.hodavand@yahoo.com>
2021-05-09 13:12:51 +0430

    Create an admin user and some random user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,23 +24,21 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
+            'username' => Str::lower($this->faker->firstName()),
             'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
-    public function unverified()
+    public function admin()
     {
         return $this->state(function (array $attributes) {
             return [
-                'email_verified_at' => null,
+                'name' => 'test',
+                'username' => 'test',
+                'email' => 'test@test.com',
+                'role' => 'admin'
             ];
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $userCount = max((int)$this->command->ask('How many users would you like?', 5), 1);
+
+        // Create an admin user
+        User::factory()->admin()->create();
+        // Create $userCount normal user
+        User::factory($userCount)->create();
     }
 }
